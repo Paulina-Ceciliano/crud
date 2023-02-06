e65350fa70d699098a483ebac1d24183ea958e3b
Paulina Ceciliano <paulinaceciliano@gmail.com>
2023-02-06 10:13:36 -0600

    método listar

---
+++ assets/js/modulos/lista.usuarios.js
@@ -0,0 +1,70 @@
+var vista = {
+    controles:{
+        tbodyListaUsuarios: $('#tablaListaUsuarios tbody')
+    },
+    init: function (){
+        vista.eventos();
+        vista.peticiones.listaUsuarios();
+    },
+    eventos: function (){},
+    callbacks: {
+        eventos: {},
+        peticiones: {
+            listarUsuarios:{
+                beforeSend: function (){
+                    var tbody = vista.controles.tbodyListaUsuarios;
+                    tbody.html(vista.utils.templates.consultando());
+                },
+                completo: function (respuesta){
+                    var tbody = vista. controles.tbodyListaUsuarios;
+                    var datos = __app.parsearRespuesta(respuesta);
+                    if(datos && datos.length > 0){
+                        tbody.html('');
+                        for (var i = 0; i < datos.length; i++){
+                            var dato = datos[i];
+                            tbody.append(vista.utils.templates.item(dato));
+                        }
+
+                    }else{
+                        tbody.html(vista.utils.templates.noHayRegistros());
+                    }
+                }
+            }
+        }
+    },
+    peticiones:{
+        listaUsuarios: function(){
+            __app.get(RUTAS_API.USUARIOS.LISTAR)
+                .beforeSend(vista.callbacks.peticiones.listarUsuarios.beforeSend)
+                .success(vista.callbacks.peticiones.listarUsuarios.completo)
+                .error(vista.callbacks.peticiones.listarUsuarios.completo)
+                .send();
+        }
+    },
+    utils:{
+        templates:{
+            item:function(obj){
+                return '<tr>'
+                    +'<td>'+ obj.nombre +'</td>'
+                    +'<td>'+ obj.apellido +'</td>'
+                    +'<td>'+ obj.username +'</td>'
+                    +'<td>'+ obj.correo +'</td>'
+                    +'<td>'+ obj.fecha +'</td>'
+                    +'<td>'+ obj.estatus +'</td>'
+                    +'<td>'
+                    +'<a href="javascript:;" class="btn-accion editar">Editar</a>'
+                    +' | '
+                    +'<a href="javascript:;" class="btn-accion eliminar">Eliminar</a>'
+                    +'<td>'
+                    +'<tr>'
+            },
+            consultando: function (){
+                return '<tr><td colspan="6">Consultando...</td></tr>'
+            },
+            noHayRegistros: function (){
+                return '<tr><td colspan="6">No hay registros...</td></tr>'
+            }
+        }
+    }
+};
+$(vista.init);
---
+++ resources/views/inicio.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+    <title>Instituto Gnóstico de México</title>
+    <link href="<?= URL::to("assets/bootstrap/css/bootstrap.min.css")?>" rel="stylesheet" type="text/css"/>
+</head>
+<body  data-urlbase=" <?= URL::base()?>">
+<div class="container">
+    <div class = "card mt-5">
+        <div class="card-header bg-dark text-white">
+            <h5>Proyecto</h5>
+        </div>
+        <div class="card-body">
+            <div class="btn-group">
+                <a href="<?= URL::to("crearusuario") ?>" class="btn btn-primary">Crear usuario</a>
+            </div>
+            <hr/>
+            <h4 class="card-title mb-4">Listar usuarios</h4>
+            <table class="table table-condensed table-hover table-striped" width="100%" id="tablaListaUsuarios" >
+                <thead class="bg-dark text-white">
+                <tr>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Usuario</th>
+                    <th>Correo</th>
+                    <th>Fecha</th>
+                    <th>Estatus</th>
+                    <th>Acciones</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colspan="6">Consultando...</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<script src="<?= URL::to("assets/plugins/jquery.js")?>" type="text/javascript"></script>
+<script src="<?= URL::to("assets/bootstrap/js/bootstrap.min.js")?>"  type="text/javascript"></script>
+<script src="<?= URL::to("assets/js/global/helperform.js"  )?>" type="text/javascript"></script>
+<script src="<?= URL::to("assets/js/global/rutas.api.js"  )?>" type="text/javascript"></script>
+<script src="<?= URL::to("assets/js/global/app.global.js")?>" type="text/javascript"></script>
+<script src="<?= URL::to("assets/js/modulos/lista.usuarios.js ")?>" type="text/javascript"></script>
+</body>
+</html>
